e1caa135f6ec69de4efc19a5a0de0bac6152c7a5
yonghao.fyh <yonghao.fyh@alibaba-inc.com>
2026-07-08 15:52:04 +0800

    fix

--- src/paimon/common/data/binary_row.cpp
+++ src/paimon/common/data/binary_row.cpp
@@ -30,9 +30,10 @@
 #include "paimon/memory/memory_pool.h"
 
 namespace paimon {
-const int64_t BinaryRow::FIRST_BYTE_ZERO = (SystemByteOrder() == ByteOrder::PAIMON_LITTLE_ENDIAN)
-                                               ? static_cast<int64_t>(~0xFFULL)
-                                               : static_cast<int64_t>(~(0xFFULL << 56));
+const int64_t BinaryRow::FIRST_BYTE_ZERO =
+    (SystemByteOrder() == ByteOrder::PAIMON_LITTLE_ENDIAN)
+        ? static_cast<int64_t>(~static_cast<uint64_t>(0xFF))
+        : static_cast<int64_t>(~(static_cast<uint64_t>(0xFF) << 56));
 
 const BinaryRow& BinaryRow::EmptyRow() {
     static const BinaryRow empty_row = GetEmptyRow();
