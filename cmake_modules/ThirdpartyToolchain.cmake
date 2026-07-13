fdca1ac34a4409c2655abf311f64a0eae17ae26d
Mr Dk. <mrdrivingduck@gmail.com>
2026-07-13 18:51:28 +0800

    fix(build): disable glog unwind without libunwind

--- cmake_modules/ThirdpartyToolchain.cmake
+++ cmake_modules/ThirdpartyToolchain.cmake
@@ -1890,6 +1890,8 @@ endmacro()
 
 macro(build_glog)
     message(STATUS "Building glog from source")
+    find_library(LIBUNWIND_LIBRARY NAMES unwind)
+
     set(GLOG_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/glog_ep-install")
     set(GLOG_INCLUDE_DIR "${GLOG_PREFIX}/include")
     if(${UPPERCASE_BUILD_TYPE} STREQUAL "DEBUG")
@@ -1912,6 +1914,9 @@ macro(build_glog)
         -DWITH_GTEST=OFF
         -DCMAKE_CXX_FLAGS=${GLOG_CMAKE_CXX_FLAGS}
         -DCMAKE_C_FLAGS=${GLOG_CMAKE_C_FLAGS})
+    if(NOT LIBUNWIND_LIBRARY)
+        list(APPEND GLOG_CMAKE_ARGS -DWITH_UNWIND=none)
+    endif()
 
     externalproject_add(glog_ep
                         URL ${GLOG_SOURCE_URL}
@@ -1929,7 +1934,6 @@ macro(build_glog)
 
     add_dependencies(glog glog_ep)
 
-    find_library(LIBUNWIND_LIBRARY NAMES unwind)
     if(LIBUNWIND_LIBRARY)
         target_link_libraries(glog INTERFACE ${LIBUNWIND_LIBRARY})
     endif()
